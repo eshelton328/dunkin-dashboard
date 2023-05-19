f95ee7edb9ff80421b9329728824e3a67a6775f5
eshelton328 <erik.shelton328@gmail.com>
2023-05-19 18:46:45 -0500

    [ADD] created basic layout for payments page

--- client/src/pages/Payments.jsx
+++ client/src/pages/Payments.jsx
@@ -1,10 +1,59 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Container, Stack, Button } from '@mui/material'
+import FileUploadIcon from '@mui/icons-material/FileUpload';
+import CheckIcon from '@mui/icons-material/Check';
+import ClearIcon from '@mui/icons-material/Clear';
+import LOGO from '../assets/img/dunkin-logo-large.png';
 
 const Payments = () => {
+    const [file, setFile] = useState(null);
+    const [fileConent, setFileContent] = useState([])
+
+    const handleFileUpload = (event) => {
+        const file = event.target.files[0]
+        if (file && file.type === 'text/xml') {
+            setFile(file)
+            // process file
+        } else {
+            setFile(null)
+        }
+    }
+
     return (
-        <div>
-            Payments Page
-        </div>
+        <Container>
+            <div className="logo_container">
+                <img
+                    src={LOGO}
+                    alt=""
+                    className="home_logo"
+                />
+                <h1>Payments</h1>
+            </div>
+            <div className="home_row">
+                <Stack direction="row" alignItems="center" spacing={2}>
+                    {file ?
+                        <>
+                            <Button variant="contained" component="label" startIcon={<CheckIcon />}>
+                                Make Payments
+                            </Button>
+                            <Button variant="contained" component="label" startIcon={<ClearIcon />} onClick={() => setFile(null)}>
+                                Cancel
+                            </Button>
+                        </>
+                        :
+                        <Button variant="contained" component="label" startIcon={<FileUploadIcon />}>
+                            Upload
+                            <input
+                                hidden
+                                type="file"
+                                accept=".xml"
+                                onChange={handleFileUpload}
+                            />
+                        </Button>
+                    }
+                </Stack>
+            </div>
+        </Container>
     )
 }
 
